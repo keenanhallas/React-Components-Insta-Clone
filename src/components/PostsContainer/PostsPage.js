cf16227b9c6786ecae550f67c533fb43fc1fb883
Keenan <keenanhallas@gmail.com>
2020-04-26 22:19:55 -0400

    have forgotten to commit so far - data now being passed into post component & posts showing up onscreen.

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,35 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import dummyData from "../../dummy-data";
+
+// dummyData.map(() => {
+//   console.log("test");
+// });
 
 const PostsPage = () => {
   // set up state for your data
+  const [postData, setPostData] = useState(dummyData);
+  console.log(postData);
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        postData.map((post) => {
+          //console.log(post);
+          return <Post post={post}/>
+        })
+      }
     </div>
   );
 };
 
 export default PostsPage;
+
+// postData.map(() => {
+//   console.log("Test")
+//   //return <Post post={post}/>;
+// });})
