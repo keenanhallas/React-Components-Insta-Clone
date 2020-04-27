fd6e0166e750132e7399d931f8f29b415e3948c1
Keenan <keenanhallas@gmail.com>
2020-04-27 02:10:27 -0400

    mvp functionality complete

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,7 +9,6 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-  console.log(props);
   const [likes, setLikes] = useState(props.post.likes);
 
   return (
